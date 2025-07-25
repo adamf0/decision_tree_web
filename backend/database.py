23357544839b8c4d35b6e71dd5bce024358114b1
!Github@251423! <studiocode52@gmail.com>
2025-07-25 11:28:54 +0700

    update

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Ganti dengan detail MariaDB di XAMPP lokal
-DATABASE_URL = "mysql+pymysql://sibea:s1b34unp4kAPPS@localhost:3306/unpak_sibea"
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://sibea:s1b34unp4kAPPS@localhost:3306/unpak_sibea")
 
 # Jika root punya password, ganti menjadi: mysql+pymysql://root:password@127.0.0.1:3306/decision_tree
 
